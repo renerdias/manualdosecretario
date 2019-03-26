3a405257ef744913809a37196c3393201bbcde67
Rener <renerdias@msn.com>
2019-03-26 15:22:02 -0300

    26/03/2019

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env sh
 
+
+SERVER=200.129.17.30
+USER=operador
+DIST=docs.ifce.edu.br
+
+
+
+
 # abort on errors
 #set -e
 
 # build
+echo "-- build --"
 vuepress build
+
+echo "-- deploy --"
+
+
+echo "-- Movendo logo --"
+cp -R ./assets/images/manual.svg ./docs/assets/img/manual.svg
+
+echo "-- Movendo arquivos de formulário --"
+cp -R ./guides/formulario/arquivos/ ./docs/guides/formulario/arquivos/
+
 
 # navigate into the build output directory
 #cd docs/
@@ -13,13 +32,13 @@ vuepress build
 # echo 'www.example.com' > CNAME
 
 #git init
-git add -A
-git commit -m 'deploy'
+#git add -A
+#git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:renerdias/manualdosecretario.git master:gh-pages
+#git push -f git@github.com:renerdias/manualdosecretario.git master:gh-pages
 
 #cd -
